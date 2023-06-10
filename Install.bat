7ec436a87973f648912389211f50cf409cbe6030
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-06-10 17:02:39 +0300

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,22 @@
+color a
+cd C:\Users\%USERNAME%\Downloads
+curl https://ldcdn.ldmnq.com/download/package/LDPlayer9.0.exe?n=LDPlayer9.0_ens_1001_ld.exe -o LDPlayer9.exe
+mkdir AndroidLaunchers
+cd C:\Users\%USERNAME%\Downloads\AndroidLaunchers
+curl https://f-droid.org/repo/fr.neamar.kiss_206.apk -o kiss.apk
+curl https://f-droid.org/repo/ch.deletescape.lawnchair.plah_2001.apk -o lawnchair.apk
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+echo Install your launcher of choice when the installation is done, Have fun!
+C:\Users\%USERNAME%\Downloads\LDPlayer9.exe
